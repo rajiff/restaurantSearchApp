225379cef5e58d0fc265a0bc57c17b556e719860
Basavaraj K N <Basavaraj.K@Stackroute.in>
2018-02-21 20:34:00 +0530

    Minor change in margins for restaurant cards

--- webapp/src/components/RestaurantThumbnail.js
+++ webapp/src/components/RestaurantThumbnail.js
@@ -20,8 +20,7 @@ export default class RestaurantThumbnail extends React.Component {
 
   render() {
   	return (
-      <Card style={{margin: '4px'}}>
-        <CardHeader />
+      <Card style={{margin: '5px'}}>
         <CardMedia overlay={<CardTitle title={this.props.restaurant.name}/>}>
           <img src={this.props.restaurant.featured_image || 'http://static.tui.co.uk/static-images/images/thomson/default-large.png'} alt='' />
         </CardMedia>
